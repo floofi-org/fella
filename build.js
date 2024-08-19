98429ef2851857504497d165b1e40e6ce11a75f4
Starscouts <starscouts@equestria.dev>
2024-08-19 19:15:21 +0200

    Initial commit

---
+++ build.js
@@ -0,0 +1,31 @@
+(async () => {
+    const fs = require('fs');
+    const csso = require('csso');
+    const files = fs.readdirSync("./src").filter(i => i.endsWith(".css"));
+    const version = fs.readFileSync("./version.txt").toString().trim();
+
+    let output = "@charset \"UTF-8\";\n\n/*!\n * Fella version " + version + "\n * Copyright Floofi Systems\n */\n\n";
+
+    for (let file of files) {
+        output += "/* " + file + " */\n";
+        output += fs.readFileSync("./src/" + file).toString().trim();
+        output += "\n\n";
+    }
+
+    if (fs.existsSync("./dist/" + version)) {
+        console.error("Version already exists.");
+        return;
+    }
+
+    fs.mkdirSync("./dist/" + version);
+    fs.writeFileSync("./dist/" + version + "/fella.css", output);
+
+    let minified = csso.minify(output, {
+        filename: 'fella.min.css', // will be added to source map as reference to source file
+        sourceMap: true             // generate source map
+    });;
+    fs.writeFileSync("./dist/" + version + "/fella.min.css", minified.css + "\n/*# sourceMappingURL=fella.min.css.map */");
+    fs.writeFileSync("./dist/" + version + "/fella.min.css.map", minified.map.toString());
+
+    require('child_process').execSync("vercel --prod", { stdio: "inherit", cwd: "./dist" });
+})();
